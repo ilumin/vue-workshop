1aa15a4f8dcfa91a326b11b559eb5b699e4a7947
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-18 09:16:30 +0700

    UPDATE product list template

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,5 +1,20 @@
 <template lang="html">
-  <h1 class="header">Vue's component</h1>
+  <table class="table table-hover product-table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="product in products" track-by="id">
+        <td>{{product.name}}</td>
+        <td>{{product.description}}</td>
+        <td>{{product.price}}:-</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
