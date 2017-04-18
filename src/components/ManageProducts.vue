2c75e10ad260a66445eb20a20c570f01a1aeef01
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-18 09:35:29 +0700

    UPDATE product list component - product list component use data from props - manage product inject products into product list props

--- src/components/ManageProducts.vue
+++ src/components/ManageProducts.vue
@@ -4,7 +4,9 @@
       :product="productInForm"
       v-on:submit="onFormSave"
     ></save-product-form>
-    <product-list></product-list>
+    <product-list
+      :products="products"
+    ></product-list>
   </section>
 </template>
 
@@ -19,7 +21,29 @@ const initialData = () => {
       name: '',
       description: '',
       price: null,
-    }
+    },
+    products: [
+      {
+        id: 'cc919e21-ae5b-5e1f-d023-c40ee669520c',
+        name: 'COBOL 101 vintage',
+        description: 'Learn COBOL with this vintage programming book',
+        price: 399,
+      },
+      {
+        id: 'bcd755a6-9a19-94e1-0a5d-426c0303454f',
+        name: 'Sharp C2719 curved TV',
+        description: 'Watch TV like never before with the brand new curved ' +
+          'screen technology',
+        price: 1995,
+      },
+      {
+        id: '727026b7-7f2f-c5a0-ace9-cc227e686b8e',
+        name: 'Remmington X mechanical keyboard',
+        description: 'Excellent for gaming and typing, this Remmington X ' +
+          'keyboard features tactile, clicky switches for speed and accuracy',
+        price: 595,
+      }
+    ]
   }
 }
 
