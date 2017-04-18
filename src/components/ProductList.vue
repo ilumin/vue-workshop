57f7580b5e10ce553cb2545b547547aeceeed249
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-18 18:37:32 +0700

    UPDATE product list component - add emit remove event

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -5,6 +5,7 @@
         <th>Name</th>
         <th>Description</th>
         <th>Price</th>
+        <th>&nbsp;</th>
       </tr>
     </thead>
     <tbody>
@@ -12,6 +13,7 @@
         <td>{{product.name}}</td>
         <td>{{product.description}}</td>
         <td>{{product.price}}:-</td>
+        <td><a href="#" v-on:click.prevent.stop="onRemove(product)">remove</a></td>
       </tr>
     </tbody>
   </table>
@@ -23,6 +25,9 @@ export default {
   methods: {
     onEdit(product) {
       this.$emit('edit', product);
+    },
+    onRemove(product) {
+      this.$emit('remove', product);
     }
   }
 }
