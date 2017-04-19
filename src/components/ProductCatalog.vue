e163026ebfc52b06c2eeec391c15d43fdb263b12
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-19 09:30:40 +0700

    ADD vuex store name products - add products store - implement products list in product catalog

--- src/components/ProductCatalog.vue
+++ src/components/ProductCatalog.vue
@@ -1,11 +1,18 @@
 <template lang="html">
-  <div>
-    PRODUCT CATALOG
-  </div>
+  <section>
+    <ul v-for="product in products" track-by="id">
+      <li>{{product.name}}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
+  computed: mapGetters({
+    products: 'getProducts'
+  })
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App'
+import store from './vuex/store'
 
 import ProductCatalog from './components/ProductCatalog'
 import ManageProducts from './components/ManageProducts'
@@ -18,6 +19,7 @@ const router = new VueRouter({
 
 new Vue({
   el: '#app',
+  store,
   router,
   render: h => h(App),
 })
